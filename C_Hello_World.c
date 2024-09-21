b62c116c6bd8f9ac542121c29872e3a8bd46cf3e
Tanishka Bhatti <tanishkabhatti7@gmail.com>
2024-09-22 00:06:19 +0530

    C Hello World Running in Local

--- C_Hello_World.c
+++ C_Hello_World.c
@@ -1,6 +1,8 @@
-#include <stdio.h>
+#include<stdio.h>
 int main()
 {
     printf("Hello, World!");
-    printf("Hello, World Again!");
+    printf("Hello, World Again!\n");
+    printf("Hello, World Again & Again in front of Tanishka :)");
+    return 0;
 }
